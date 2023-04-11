043da11599204837c689980d6d8b19886e516f36
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-04-11 16:42:43 -0700

    working with tutor needing to push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
-import Home from './components/Home';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Resume from './components/Resume';
-import Contact from './components/ContactMe';
+import Home from './components/pages/Home';
+import AboutMe from './components/pages/AboutMe';
+import Portfolio from './components/pages/Portfolio';
+import Resume from './components/pages/Resume';
+import Contact from './components/pages/ContactMe';
 import Footer from './components/Footer';
 
 function App() {

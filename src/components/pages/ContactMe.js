a9bac7880c9a444573101ac5cdb947e131861e76
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-06 07:58:04 -0700

    added the last functionality to the contact me where a deselection of the name text field is made and it has no contents

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function ContactMe() {ç
+function ContactMe() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+
+  const handleOnChange = (e) => {
+    setInputs(() => ({
+      [e.target.name]: e.target.value
+    }))
+    console.log("e", e.target.value)
+    console.log("inputs.name", inputs.name)
+  }
+
   return (
     <div>
       <h1>
         Contact Me
-        </h1>
-        <div>
-          <form>
-            <input type="text" placeholder='name'/>
-            <input type="email" placeholder='email'/>
-            <textarea type="text" placeholder='message'/>
-            <button type='submit'>
-              Contact Me
-            </button>
-          </form>
-        </div>
+      </h1>
+      <div>
+        <form>
+          {inputs.name === "" && (
+            <div>please enter name</div>
+          )}
+          <input type="text" placeholder='name' name="name"
+            value={inputs.name} onChange={handleOnChange}
+          />
+          <input type="email" placeholder='email' />
+          <textarea type="text" placeholder='message' />
+          <button type='submit'>
+            Contact Me
+          </button>
+        </form>
       </div>
+    </div>
   )
 }
 

0bd6e01f0b24b4ba9be0f1ea167b94de620746f8
[Jen ny] <[jengutierrez.1210@gmail.com]>
2023-05-18 11:18:53 -0700

    added spacing within home content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -12,10 +12,13 @@ function Home() {
 
                 <p>
                     My name is Jennifer, but I prefer to go by Jenni. I hold a dual degree in Management Information Systems and Marketing from Washington State University. During my studies, I rediscovered my love for coding, which I had first experienced during an HTML web-design course I took a few years prior at Wenatchee Valley College. As of May 1st of 2023, I've completed the UW Fullstack Coding Bootcamp that taught the most up-to date practices in website development.
+                    <br />
 
                     <br />
                     To kick off my career in tech, I opted for an IT focused role to widen my understanding of technological hardware. I'm a data center technician for T-Mobile and although I have been loving getting to work hands-on with technological hardware, I still have a burning passion to pursue software engineering. I feel confident that the bootcamp I attended has provided me with the fundamental tools to tackle on any problem or need that arises.
                     <br />
+                    <br />
+
                     If you're curious to learn more then feel free to explore the rest of my portfolio which is still under development.
 
                 </p>
